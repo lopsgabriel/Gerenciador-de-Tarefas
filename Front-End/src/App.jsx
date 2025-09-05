bc0a782cda7511e80db1a469ce6e499874dff860
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-09-05 16:50:02 -0300

    :sparkles: feat: roteamento da aplicação

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Tarefas from './pages/Tarefas';
+import Login from './pages/Login';
+import ProtectedRouter from './components/Router';
+
+export default function App(){
+  return(
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<ProtectedRouter><Tarefas /></ProtectedRouter>} />
+    </Routes>
+  )
+}
---
+++ Front-End/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)
