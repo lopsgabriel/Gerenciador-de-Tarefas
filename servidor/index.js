65d32e011bdf9d07bd1946a761bce557304dc35d
Gabriel Lopes <souzsgabriel12@gmail.com>
2025-09-09 12:59:08 -0300

    :recycle: refactor: transformar o index.js em ponto de entrada

--- servidor/index.js
+++ servidor/index.js
@@ -1,2 +1,7 @@
 require('dotenv').config();
-require('./src/server');
+const { PORTA } = require('./src/config');
+const app =require('./src/server');
+
+if (require.main === module) {
+  app.listen(PORTA, () => console.log(`Servidor rodando na porta ${PORTA}`));
+}
